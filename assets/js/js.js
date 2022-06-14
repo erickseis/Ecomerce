dbed4c93633af220daaccebddfd203864be992b1
unknown <erickseis@gmail.com>
2022-06-14 17:24:31 -0400

    a

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,8 +2,13 @@
 
 let cartIcon = document.querySelector(".cart")
 let cartOverlay = document.querySelector(".shopping-cart-overlay")
+let cartClose = document.getElementById("cart-close")
 
 
 cartIcon.addEventListener("click", () =>{
     cartOverlay.classList.add("mostrar")
+})
+
+cartClose.addEventListener("click", () =>{
+    cartOverlay.classList.remove("mostrar")
 })
